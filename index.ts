701cc54c0f45fa7c43e5b358e825e6a10065955c
KachiiC <kacheokcheong@gmail.com>
2022-05-09 13:28:47 +0100

    added parser and cors

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
-import express, { Request, Response } from 'express';
+import Express, { json } from 'express';
 import router from './router';
+import cors from 'cors'
 
-const app = express();
+const app = Express();
 const PORT = 8000
 
-app.use(router)
+app.use(cors())
+    .use(json())
+    .use(router)
 
 app.listen(PORT, () => {
     console.log(`The application is listening on port http://127.0.0.1:${PORT}/`);
