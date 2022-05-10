5a701fb97690c86fa5c714fb7c6be178b2af2cfb
KachiiC <kacheokcheong@gmail.com>
2022-05-10 18:07:35 +0100

    edit crypto controllers

--- src/controllers/crypto.controllers.ts
+++ src/controllers/crypto.controllers.ts
@@ -1,8 +1,24 @@
-// DATA
-import cryptoMockData from "../data/crypto.data"
 // TYPES
 import { Request, Response } from 'express'
+import { cryptoHeaders } from '../helpers/headers'
+import { cryptoUrl } from '../helpers/urls'
+import { getRequestWithHeaders } from "../helpers/apiRequests"
 
-export const getUserCrypto = (req: Request, res: Response) => {
-    res.send(cryptoMockData)
+export const getUserCrypto = async (req: Request, res: Response) => {
+
+    const url = cryptoUrl(req.params.cryptolist)
+
+    try {
+        const data = await getRequestWithHeaders(
+            url,
+            cryptoHeaders(process.env.COINCAP_KEY || "")
+        )
+
+        res.send(data.data)
+    }
+    catch (err) {
+        console.error(err)
+        res.sendStatus(404)
+    }
+
 }
