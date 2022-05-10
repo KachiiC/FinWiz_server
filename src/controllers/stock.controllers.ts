abbf18728d7ea8c5042a3096c5a62017e48690dc
KachiiC <kacheokcheong@gmail.com>
2022-05-10 18:07:14 +0100

    edit stock controllers

--- src/controllers/stock.controllers.ts
+++ src/controllers/stock.controllers.ts
@@ -1,8 +1,31 @@
-// DATA
-import stockMockData from "../data/stock.data"
 // TYPES
 import { Request, Response } from 'express'
+import { iexApiStockList, iexApiStockQuotes } from "../helpers/urls"
+import { getRequest } from "../helpers/apiRequests"
 
-export const getUserStocks = (req: Request, res: Response) => {
-    res.send(stockMockData)
+export const getUserStocks = async (req: Request, res: Response) => {
+
+    const url = iexApiStockQuotes(req.params.stocklist)
+
+    try {
+        const data = await getRequest(url)
+        
+        res.send(data.data)
+    } catch (err) {
+        console.error(err)
+        res.sendStatus(404)
+    }
+}
+
+export const getStockList = async (req: Request, res: Response) => {
+
+    const url = iexApiStockList(req.params.type)
+    try {
+        const data = await getRequest(url)
+
+        res.send(data.data)
+    } catch (err) {
+        console.error(err)
+        res.sendStatus(404)
+    }
 }
