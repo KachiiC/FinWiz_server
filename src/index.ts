719df9dae7dd9c89d2f4e20ecb113db14ca98e2b
Silky Ng <silky.ng@gmail.com>
2022-05-13 16:40:27 +0100

    feat(morgan): added morgan logging

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import dotenv from "dotenv"
 import Express, { json } from 'express'
 import router from './router'
 import cors from 'cors'
+import morgan from 'morgan'
 
 dotenv.config()
 const App = Express()
 const PORT = process.env.PORT || 8000
 
 App.use(cors())
+    .use(morgan('short'))
     .use(json())
     .use(router)
 
