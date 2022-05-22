341331a6f0adfe92d607d267c3103b1e36e5828a
Silky Ng <silky.ng@gmail.com>
2022-05-22 15:51:15 +0100

    feat(apiRequests): added some testing

---
+++ src/helpers/tests/apiRequests.test.ts
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import { test, jest, expect } from '@jest/globals'
+import { getRequest, getRequestWithHeaders } from '../apiRequests'
+
+const cryptoData = {
+  BTC: {
+    quote: {
+      USD: { price: 30000 }
+    },
+    name: 'BitCoin'
+  }
+}
+
+jest.mock('axios')
+
+test('should return response from getRequest ', async () => {
+  const URL = 'http://test.com'
+  // @ts-ignore
+  axios.request.mockResolvedValueOnce({ data: cryptoData })
+  const response = await getRequest(URL)
+  expect(axios.request).toHaveBeenCalledWith({ method: 'GET', url: URL })
+  expect(response).toEqual({ data: cryptoData })
+})
+
+test('shoud return response from getRequestWithHeaders ', async () => {
+  const URL = 'http://test.com'
+  // @ts-ignore
+  axios.request.mockResolvedValueOnce({ data: cryptoData })
+  const response = await getRequestWithHeaders(URL, { test: 'header' })
+  expect(axios.request).toHaveBeenCalledWith({ method: 'GET', url: URL, headers: { test: 'header' } })
+  expect(response).toEqual({ data: cryptoData })
+})
