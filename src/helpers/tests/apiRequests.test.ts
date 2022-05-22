c7875dccb1c176cb4b637812bd16aef4e6589c31
Silky Ng <silky.ng@gmail.com>
2022-05-22 16:45:30 +0100

    feat(apiRequests): tests for ApiRequests

--- src/helpers/tests/apiRequests.test.ts
+++ src/helpers/tests/apiRequests.test.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { test, jest, expect } from '@jest/globals'
-import { getRequest, getRequestWithHeaders } from '../apiRequests'
+import { getRequest, getRequestWithHeaders, cryptoApiData, stockApiData, spreadArgs } from '../apiRequests'
 
 const cryptoData = {
   BTC: {
@@ -29,4 +29,24 @@ test('shoud return response from getRequestWithHeaders ', async () => {
   const response = await getRequestWithHeaders(URL, { test: 'header' })
   expect(axios.request).toHaveBeenCalledWith({ method: 'GET', url: URL, headers: { test: 'header' } })
   expect(response).toEqual({ data: cryptoData })
+})
+
+test('should call get request with crypto api data', async () => {
+  // @ts-ignore
+  axios.request.mockResolvedValueOnce({ data: cryptoData })
+  const response = await cryptoApiData('BTC')
+  expect(response).toEqual(cryptoData)
+})
+
+test('should call get request with stock api data', async () => {
+  // @ts-ignore
+  axios.request.mockResolvedValueOnce({ data: cryptoData })
+  const response = await stockApiData('BTC')
+  expect(response).toEqual({ data: cryptoData })
+})
+
+test('should spread an array of symbols into a string', () => {
+  const dataArray = [{ symbol: 'BTC' }, { symbol: 'DOGE' }, { symbol: 'XLM' }]
+  const result = 'BTC,DOGE,XLM'
+  expect(spreadArgs(dataArray)).toEqual(result)
 })
